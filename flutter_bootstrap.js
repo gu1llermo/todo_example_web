9b67d8db96237fb067c1a91aa7f20d00d52d176b
GIND <guillermo6001@gmail.com>
2024-09-16 20:54:16 -0400

    mejora en los metodos de sembast datasource

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "524440484"
+    serviceWorkerVersion: "1220226753"
   }
 });
